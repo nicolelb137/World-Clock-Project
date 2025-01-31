db5ee2e0110ed774ce24c6b9873d964d5041c963
nicolelb137 <nicolelb137@gmail.com>
2025-01-31 14:18:41 -0500

    fixed css & html and added javascript

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+function updateTime() {
+  let chicagoElement = document.querySelector("#chicago");
+  let chicagoDateElement = chicagoElement.querySelector(".date");
+  let chicagoTimeElement = chicagoElement.querySelector(".time");
+  let chicagoTime = moment().tz("America/Chicago");
+
+  chicagoDateElement.innerHTML = chicagoTime.format("MMMM Do YYYY");
+  chicagoTimeElement.innerHTML = chicagoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let denverElement = document.querySelector("#denver");
+  let denverDateElement = denverElement.querySelector(".date");
+  let denverTimeElement = denverElement.querySelector(".time");
+  let denverTime = moment().tz("America/Denver");
+
+  denverDateElement.innerHTML = denverTime.format("MMMM Do YYYY");
+  denverTimeElement.innerHTML = denverTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
